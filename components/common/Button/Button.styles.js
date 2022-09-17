c989d02bfedeba585df10ab2f16913731a4547cd
richard483 <richard.william483@gmail.com>
2022-09-17 21:33:41 +0700

    feat: added responsive signup pages

--- components/common/Button/Button.styles.js
+++ components/common/Button/Button.styles.js
@@ -2,34 +2,34 @@ import styled from "styled-components";
 import { COLORS } from "../../../constants/styles";
 
 export const Wrapper = styled.button`
-    background: ${props => props.color ? props.color : COLORS.pink};
-    border: none;
-    border-radius: .25rem;
+  background: ${(props) => (props.color ? props.color : COLORS.pink)};
+  border: none;
+  border-radius: 0.25rem;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: 0.5rem;
+  padding: 1rem 2rem;
+  color: ${COLORS.white};
+  font-size: 1rem;
+  font-weight: 500;
+  cursor: pointer;
+
+  :hover {
+    background: #c75a8f;
+  }
+
+  span {
+    font-size: 1.2rem;
     display: flex;
-    align-items: center;
-    justify-content: center;
-    gap: .5rem;
-    padding: 1rem 2rem;
-    color: ${COLORS.white};
-    font-size: 1rem;
-    font-weight: 500;
-    cursor: pointer;
+  }
 
-    :hover{
-        background: #C75A8F;
-    }
+  @media (max-width: 768px) {
+    font-size: 0.75rem;
+    padding: 0.75rem 1.25rem;
 
-    span{
-        font-size: 1.2rem;
-        display: flex;
+    span {
+      font-size: 1rem;
     }
-
-    @media (max-width: 768px) {
-        font-size: .75rem;
-        padding: .75rem 1.25rem;
-        
-        span{
-            font-size: 1rem;
-        }
-    }
-`
+  }
+`;
--- pages/register/index.jsx
+++ pages/register/index.jsx
@@ -1,53 +1,158 @@
-import React, { useState } from 'react'
-import { createUserWithEmailAndPassword, updateProfile } from 'firebase/auth'
-import { auth, storage } from '../../firebase/firebase'
-import { getDownloadURL, ref, uploadBytes } from 'firebase/storage';
+import React, { useState } from "react";
+import {
+  createUserWithEmailAndPassword,
+  updateProfile,
+  GoogleAuthProvider,
+  signInWithPopup,
+} from "firebase/auth";
+import { auth, storage } from "../../firebase/firebase";
+import { getDownloadURL, ref, uploadBytes } from "firebase/storage";
+import Link from "next/link";
+import Image from "next/image";
+import {
+  ContentWrapper,
+  FormWrapper,
+  LogoWrapper,
+  Title,
+  Wrapper,
+  LeftFormWrapper,
+  StyledButton,
+  FileLabelWrapper,
+  SignUpWrapper,
+  ImgWrapper,
+} from "./index.styles";
 
 function Register() {
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
-    const [profilePic, setProfilePic] = useState("");
-    const [displayName, setDisplayName] = useState("");
-    const [photoURL, setPhotoURL] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [reenterpassword, setreenterpassword] = useState("");
+  const [profilePic, setProfilePic] = useState("");
+  const [displayName, setDisplayName] = useState("");
+  const [photoURL, setPhotoURL] = useState("");
+  const [user, setUser] = useState();
+  const provider = new GoogleAuthProvider();
 
-    const register = async () => {
-        try {
-            await createUserWithEmailAndPassword(auth, email, password, displayName)
-                .catch((err) => alert(err.message)
-                )
-            await updateProfile(auth.currentUser, { displayName, photoURL })
-                .catch((err) => alert(err.message)
-                )
-            setProfilePicture()
-            alert('SUCCESSFULLY REGISTERED')
-        } catch (err) {
-            alert(err.message)
-        }
+  const register = async () => {
+    try {
+      await createUserWithEmailAndPassword(
+        auth,
+        email,
+        password,
+        displayName
+      ).catch((err) => alert(err.message));
+      await updateProfile(auth.currentUser, { displayName, photoURL }).catch(
+        (err) => alert(err.message)
+      );
+      setProfilePicture();
+      alert("SUCCESSFULLY REGISTERED");
+    } catch (err) {
+      alert(err.message);
     }
-
-    const setProfilePicture = () => {
-        const key = displayName + email
-        const imageRef = ref(storage, `profile/${key}`)
-        uploadBytes(imageRef, profilePic).then((snapshot) => {
-            getDownloadURL(snapshot.ref).then((url) => {
-                setPhotoURL(url)
-            })
-        })
-        alert('Profile Picture Uploaded')
+  };
+  const registerWithGoogle = async () => {
+    try {
+      await signInWithPopup(auth, provider).catch((err) => alert(err.message));
+      alert("SUCCESSFULLY REGISTERED");
+    } catch (err) {
+      alert(err.message);
     }
+  };
 
-    return (
-        <div>
-            <h1>Register</h1>
-            <input type="file" onChange={(e) => setProfilePic(e.target.files[0])} />
-            {/* ijin apus value di atas, biar ga error */}
-            <input type="text" value={displayName} onChange={(e) => setDisplayName(e.target.value)} />
-            <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} placeholder='Email' required />
-            <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} placeholder='Password' required />
-            <button onClick={register}>Register</button>
-            <button onClick={setProfilePicture}>Upload</button>
-        </div>
-    )
+  const setProfilePicture = () => {
+    const key = displayName + email;
+    const imageRef = ref(storage, `profile/${key}`);
+    uploadBytes(imageRef, profilePic).then((snapshot) => {
+      getDownloadURL(snapshot.ref).then((url) => {
+        setPhotoURL(url);
+      });
+    });
+    alert("Profile Picture Uploaded");
+  };
+
+  return (
+    <Wrapper>
+      <Link href="/">
+        <LogoWrapper>
+          <img src="/static/assets/w3bLogoLight.png" alt="W3B Logo" />
+        </LogoWrapper>
+      </Link>
+
+      <Title>Sign Up</Title>
+      <ContentWrapper>
+        <LeftFormWrapper>
+          <ImgWrapper>
+            <img src="/static/assets/loginPpl.svg" alt="Sign Up" />
+            <img id="line" src="/static/assets/loginLine.svg" alt="Sign Up" />
+          </ImgWrapper>
+        </LeftFormWrapper>
+        <FormWrapper>
+          <label htmlFor="display-name">Name</label>
+          <input
+            id="display-name"
+            type="text"
+            value={displayName}
+            onChange={(e) => setDisplayName(e.target.value)}
+            required
+          />
+
+          <label htmlFor="email">Email</label>
+          <input
+            id="email"
+            type="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            required
+          />
+
+          <label htmlFor="password">Password</label>
+          <input
+            id="password"
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            required
+          />
+
+          <label htmlFor="re-enter-password">Re-enter Password</label>
+          <input
+            id="reenterpassword"
+            type="password"
+            value={reenterpassword}
+            onChange={(e) => setreenterpassword(e.target.value)}
+            required
+            style={{ marginBottom: "2rem" }}
+          />
+
+          {!profilePic ? (
+            <FileLabelWrapper htmlFor="profile-picture">
+              <img src="/static/assets/uploadIcon.png" />
+              Upload your Profile Picture
+            </FileLabelWrapper>
+          ) : (
+            <FileLabelWrapper htmlFor="profile-picture">
+              <img src="/static/assets/uploadIcon.png" />
+              Upload {profilePic.name}
+            </FileLabelWrapper>
+          )}
+
+          <input
+            id="profile-picture"
+            type="file"
+            onChange={(e) => setProfilePic(e.target.files[0])}
+          />
+
+          <SignUpWrapper>
+            <StyledButton onClick={register}>Sign Up</StyledButton>
+            <div>atau</div>
+            <FileLabelWrapper onClick={registerWithGoogle}>
+              <img src="/static/assets/googleIcon.png" alt="Google" />
+              Daftar dengan Google
+            </FileLabelWrapper>
+          </SignUpWrapper>
+        </FormWrapper>
+      </ContentWrapper>
+    </Wrapper>
+  );
 }
 
-export default Register
+export default Register;
