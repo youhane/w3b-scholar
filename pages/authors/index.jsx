17c3df1a82557a7eea2eac168520e2db798890a7
youhane <jason.sebastian154@gmail.com>
2022-10-10 11:40:07 +0700

    fix: temporary fix merge to main

--- pages/authors/index.jsx
+++ pages/authors/index.jsx
@@ -4,17 +4,54 @@ import { useRouter } from "next/router";
 import React, { useEffect, useState } from "react";
 import Searchbar from "../../components/common/Searchbar/Searchbar";
 import AuthorCard from "../../components/pages/Authors/AuthorCard";
-import { Wrapper, AuthorWrapper } from "./authors.style";
+
+const Wrapper = styled.div`
+  .author-list {
+    display: grid;
+    grid-template-columns: auto auto auto;
+    grid-row-gap: 3rem;
+    grid-column-gap: 1rem;
+    justify-items: center;
+    padding: 2rem 0.1rem;
+  }
+
+  @media (max-width: 1024px) {
+    .author-list {
+      width: 80%;
+      grid-template-columns: auto auto;
+      grid-row-gap: 1.5rem;
+      grid-column-gap: 0rem;
+    }
+
+    .author-list-container {
+      display: flex;
+      justify-content: center;
+    }
+  }
+
+  @media (max-width: 920px) {
+    .author-list {
+      width: 90%;
+    }
+  }
+
+  @media (max-width: 767px) {
+    .author-list {
+      width: 100%;
+    }
+  }
+`;
+
+const AuthorWrapper = styled.div`
+  background-color: white !important;
+  color: #394955;
+`;
 
 const Authors = (props) => {
   const router = useRouter();
   const pathName = router.pathname;
 
   const [authors, setAuthors] = useState([]);
-
-  // useEffect(() => {
-  //   setAuthors(getServerSideProps());
-  // }, []);
 
   const data = {
     data: [
@@ -44,11 +81,11 @@ const Authors = (props) => {
       },
     ],
   };
-  
+
   const property = {
     data: [],
   };
-  
+
   for (let i = 0; i < 10; i++) {
     property.data.push(...data.data);
   }
